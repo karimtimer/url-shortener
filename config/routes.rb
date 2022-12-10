62cf52dc18799cd7558eeb2272028425b483b980
karimtimer <abdikarimtimer@gmail.com>
2022-12-10 20:02:55 +0000

    Show short url as redirected link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :urls
   get ':id', to: 'urls#redirect'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'urls#index'
 end
