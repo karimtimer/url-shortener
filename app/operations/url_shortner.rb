a6d7cd0fa3b0b2800358630adb050c3ca4f3bd85
karimtimer <abdikarimtimer@gmail.com>
2022-12-10 17:45:49 +0000

    Create specs for UrlShortner operation

---
+++ app/operations/url_shortner.rb
@@ -0,0 +1,6 @@
+class URLShortner
+  class << self
+    def call(long_url:)
+    end
+  end
+end
---
+++ spec/operations/url_shortner_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe URLShortner do
+
+  describe '.call' do
+    subject { URLShortner.call(long_url: long_url) }
+
+    context 'given long_url is not a valid url' do
+      let(:long_url) { 'foo' }
+
+      it 'returns false for successful'
+      it 'does not create a new url object'
+      it 'returns an error message'
+    end
+
+    context 'given long_url is a valid url' do
+      let(:long_url) { 'https://example.com/' }
+
+      context 'when the long_url has been shortened' do
+        it 'returns true for successful'
+        it 'does not create a new url object'
+        it 'returns a url object with both a long_url and a short_url'
+      end
+
+      context 'when the long_url has not already been shortened' do
+        it 'returns true for successful'
+        it 'does create a new url object'
+        it 'returns a url object with both a long_url and a short_url'
+      end
+    end
+  end
+end
